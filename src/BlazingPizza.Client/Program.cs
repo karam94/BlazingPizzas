362cd83a0afa49e90c323e17aea8c8b4602a6ecb
Karam Kabbara <contact@karam.io>
2020-06-13 21:26:32 +0100

    Order preserved in localstorage

--- src/BlazingPizza.Client/Program.cs
+++ src/BlazingPizza.Client/Program.cs
@@ -18,7 +18,7 @@ namespace BlazingPizza.Client
             builder.Services.AddScoped<OrderState>();
 
             // Add auth services
-            builder.Services.AddApiAuthorization();
+            builder.Services.AddApiAuthorization<PizzaAuthenticationState>();
             builder.Services.AddHttpClient<OrdersClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
