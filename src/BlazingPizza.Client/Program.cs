128435518c94a4fe53f144e07ebfbeae766707ab
Karam Kabbara <contact@karam.io>
2020-06-13 22:23:02 +0100

    Revert "Redirect to homepage when logging out"
    This reverts commit 0cd7dbe93fd29158f10daa02bf7c01c540bf9e2c.

--- src/BlazingPizza.Client/Program.cs
+++ src/BlazingPizza.Client/Program.cs
@@ -18,10 +18,7 @@ namespace BlazingPizza.Client
             builder.Services.AddScoped<OrderState>();
 
             // Add auth services
-            builder.Services.AddApiAuthorization<PizzaAuthenticationState>(options =>
-            {
-                options.AuthenticationPaths.LogOutSucceededPath = "";
-            });
+            builder.Services.AddApiAuthorization<PizzaAuthenticationState>();
             builder.Services.AddHttpClient<OrdersClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
